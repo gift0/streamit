3985166fbaf53cfc4624f78ff97b97910acffc9d
Gift Maduabuchi <giftmaduabuchi@gmail.com>
2025-09-22 22:08:51 +0100

    updating models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,13 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func, Float
 from sqlalchemy.orm import relationship
 from .database import Base
+
+class Bin(Base):
+    __tablename__ = "bins"
+
+    id = Column(Integer, primary_key=True, index=True)
+    location = Column(String, nullable=False, index=True)
+    latitude = Column(Float, nullable=True, index=True)
+    longitude = Column(Float, nullable=True, index=True)
+
+    reports = relationship("Report", back_populates="bin", cascade="all, delete-orphan")
