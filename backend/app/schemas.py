fbf22439b903e394eefbb2fa158b3d1341a87dd2
Gift Maduabuchi <giftmaduabuchi@gmail.com>
2025-09-22 22:19:41 +0100

    updated schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -6,3 +6,13 @@ class BinBase(BaseModel):
     location: str = Field(..., example="5th Avenue Main St")
     latitude: Optional[float] = Field(None, example=6.5244)
     longitude: Optional[float] = Field(None, example=3.3792)
+    
+class BinCreate(BinBase):
+    pass
+
+
+class BinRead(BinBase):
+    id: int
+
+    class Config:
+        from_attributes = True
