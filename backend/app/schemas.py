edc02f2a17f72ec3ccc866531f7450b398e71974
Gift Maduabuchi <giftmaduabuchi@gmail.com>
2025-09-22 22:18:36 +0100

    Created the BinBase Class on schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,3 +1,8 @@
 from datetime import datetime
 from typing import Optional, List
 from pydantic import BaseModel, Field
+
+class BinBase(BaseModel):
+    location: str = Field(..., example="5th Avenue Main St")
+    latitude: Optional[float] = Field(None, example=6.5244)
+    longitude: Optional[float] = Field(None, example=3.3792)
